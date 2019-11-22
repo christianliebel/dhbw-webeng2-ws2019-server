7b7b1842d33d8e39e6dfb058b4a56308f44ac302
Christian Liebel <christian.liebel@thinktecture.com>
2019-11-22 11:26:00 +0100

    Add auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
+import { AuthenticationMiddleware } from './common/authentication.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, TodosController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes(TodosController);
+  }
+}
